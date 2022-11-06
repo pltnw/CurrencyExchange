e0cffdee18b66d2c79355c3cd807b5dcc687ed34
pltnw <k.pltnw@gmail.com>
2022-11-06 18:17:48 +0100

    Model setup

--- CurrencyExchange/Models/CoinGecko.swift
+++ CurrencyExchange/Models/CoinGecko.swift
@@ -5,4 +5,19 @@
 //  Created by Екатерина Платонова on 06.11.2022.
 //
 
+struct Welcome: Codable {
+    let rates: [String: Rate]
+}
 
+struct Rate: Codable {
+    let name: String
+    let unit: String
+    let value: Double
+    let type: TypeEnum
+}
+
+enum TypeEnum: String, Codable {
+    case commodity = "commodity"
+    case crypto = "crypto"
+    case fiat = "fiat"
+}
