8d6be98aa817e269909f214df10f87d371545b4a
pltnw <113547510+pltnw@users.noreply.github.com>
2022-11-12 22:01:29 +0100

    set up constraints for image view

---
+++ CurrencyExchange/ViewControllers/InfoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InfoTableViewCell.swift
+//  CurrencyExchange
+//
+//  Created by Екатерина Платонова on 12.11.2022.
+//
+
+import UIKit
+
+class InfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
