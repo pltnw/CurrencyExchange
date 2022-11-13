eab01a4f1b910352ec122dd64de78d252e65e518
pltnw <113547510+pltnw@users.noreply.github.com>
2022-11-13 23:24:08 +0100

    set up manual parsing (in progress) part 3

--- CurrencyExchange/ViewControllers/ManualInfoTableViewController.swift
+++ CurrencyExchange/ViewControllers/ManualInfoTableViewController.swift
@@ -11,6 +11,7 @@ import Alamofire
 class ManualInfoTableViewController: UITableViewController {
 
     private var currencys = Currencies(rates: [String : Rate]())
+//    private var coins: [Rate] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,58 +20,52 @@ class ManualInfoTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        currencys.rates.count
-        10
+        currencys.rates.count
+//        coins.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "manualInfoCell", for: indexPath)
         guard let cell = cell as? ManualInfoTableViewCell else { return UITableViewCell() }
 
-//        let currency = Array(currencys.rates.values)[indexPath.row]
-//
-//        cell.configure(with: currency)
+        let currency = Array(currencys.rates.values)[indexPath.row]
+
+        cell.configure(with: currency)
+        
+        
+//        let coinCell = coins[indexPath.row]
+//        cell.configure(with: coinCell)
 
         return cell
     }
     
-//    private func fetchCoins() {
-//        AF.request(Link.coinGecko.rawValue)
-//            .responseJSON { dataResponse in
-//                guard let statusCode = dataResponse.response?.statusCode else { return }
-//
-//                print("Status code: \(statusCode)")
-//
-//                if (200..<300).contains(statusCode) {
-//                    guard let value = dataResponse.value else { return }
-//                    print("Value: \(value)")
-//
-//                    return
-//                }
-//
-//                guard let error = dataResponse.error else { return }
-//                print("Error: \(error)")
-//            }
-//    }
-    
     private func fetchCoins() {
         AF.request(Link.coinGecko.rawValue)
             .validate()
-            .responseJSON { dataResponse in
+            .responseJSON { [weak self] dataResponse in
                 switch dataResponse.result {
                 case .success(let value):
                     guard let coinsData = value as? [String: Any] else { return }
                     print("Value of JSON: \(coinsData)")
                     
-                   let coin = Rate(
-                            name: coinsData["name"] as? String ?? "",
-                            unit: coinsData["unit"] as? String ?? "",
-                            value: coinsData["value"] as? Double ?? 0,
-                            type: coinsData["type"] as? TypeEnum ?? .crypto
-                        )
-
-                    print("Coin: \(coin)")
+                    let coin = Currencies(rates: ["rates": Rate(
+                        name: coinsData["name"] as? String ?? "",
+                        unit: coinsData["unit"] as? String ?? "",
+                        value: coinsData["value"] as? Double ?? 0,
+                        type: coinsData["type"] as? TypeEnum ?? .crypto)])
                     
+                    self?.currencys = coin
+                    
+                    
+//                    let coin = Rate(
+//                            name: coinsData["name"] as? String ?? "",
+//                            unit: coinsData["unit"] as? String ?? "",
+//                            value: coinsData["value"] as? Double ?? 0,
+//                            type: coinsData["type"] as? TypeEnum ?? .crypto
+//                        )
+//                    self?.coins.append(coin)
+
+                    self?.tableView.reloadData()
                 case .failure(let error):
                     print(error)
                 }
